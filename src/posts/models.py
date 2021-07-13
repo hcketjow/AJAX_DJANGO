dceb98b00d9fa80d44c6e23a5c827f01e79fa8ef
Wojciech <hcketjow@gmail.com>
2021-07-13 10:47:32 +0200

    next

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from profiles.models import Profiles
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    liked = models.ManyToManyField(User, blank=True)
+    author = models.ForeignKey(Profiles, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.title)
