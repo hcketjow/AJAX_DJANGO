2eb2bd30deb14888712e0d7017b070ee18d51cba
Wojciech <hcketjow@gmail.com>
2021-07-14 12:15:35 +0200

    najs

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,11 +1,57 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+from .forms import PostForm
+from profiles.models import Profile
 
 # Create your views here.
 def post_list_and_create(request):
+    form = PostForm(request.POST or None)
+    # qs = Post.objects.all()
+    
+    if request.is_ajax():
+        if form.is_valid():
+            author = Profile.object.get(user=request.user)
+            instance = form.save(commit=False)
+            instance.author = author
+            instance.save()
+    context = {
+        'form': form
+    }
+    return render(request, 'posts/main.html', context)
+
+def load_post_data_view(request,num_posts):
+    visible = 3
+    upper = num_posts
+    lower = upper - visible
+    size = Post.objects.all().count()
+
     qs = Post.objects.all()
-    return render(request, 'posts/main.html', {'qs': qs})
+    data = []
+    for obj in qs:
+        item = {
+            'id': obj.id,
+            'title': obj.title,
+            'body': obj.body,
+            'liked': True if request.user in obj.liked.all() else False,
+            'count': obj.like_count,
+            'author': obj.author.user.username
+        }
+        data.append(item)
+    return JsonResponse({'data':data[lower:upper], "size": size})
+
+def like_unlike_post(request):
+    if request.is_ajax():
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else: 
+            liked = True
+            obj.liked.add(request.user)
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
+
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hellow world x2'})
